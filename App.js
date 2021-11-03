c4a4f90f8ac8583b30180a1c4bf405ff0083b8e4
Shambhavi <69713501+Shambhavi-jha13@users.noreply.github.com>
2021-11-03 23:54:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { uuid } from 'uuidv4';
+import './App.css';
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from './ContactList';
+
+
+function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([])
+  
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, {id: uuid(), ...contact }]);
+  };
+  
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+
+  };
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContacts) setContacts(retriveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+  
+  return (
+    <div className="ui container">
+      <Router>
+      <Header />
+      <Switch>
+      <route path="/" exact component={ContactList} />
+        <route path="/add" component={ () => <AddContact addContactHandler={addContactHandler} />
+  }
+  />
+      </Switch>
+      
+      { /* <AddContact addContactHandler={addContactHandler }/>
+      <ContactList contacts={contacts} getContactId={removeContactHandler} /> */}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
